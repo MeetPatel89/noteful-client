b5113c31a6e47014ca42bb7b6cf79387cee1e396
Meet Patel <meetaiims@yahoo.com>
2020-12-12 03:14:39 -0600

    initial commit

---
+++ src/NoteListNav/NoteListNav.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import AddFolder from '../AddFolder/AddFolder'
+import CircleButton from '../CircleButton/CircleButton';
+import ApiContext from '../ApiContext';
+import { countNotesForFolder } from '../notes-helpers';
+import './NoteListNav.css';
+
+export default class NoteListNav extends React.Component {
+  state = {
+    adding: false,
+  };
+  static contextType = ApiContext;
+  
+
+  addFolderHandler = () => {
+    console.log("notelist nav state before: ", this.state)
+    this.setState({ adding: !this.state.adding })
+    this.forceUpdate()
+    console.log("notelist nave state: ", this.state);
+  };
+  
+  render() {
+    const { folders = [], notes = [] } = this.context;
+    return (
+      <div className='NoteListNav'>
+        <ul className='NoteListNav__list'>
+          {folders.map(folder => (
+            <li key={folder.id}>
+              <NavLink
+                className='NoteListNav__folder-link'
+                to={`/folder/${folder.id}`}
+              >
+                <span className='NoteListNav__num-notes'>
+                  {countNotesForFolder(notes, folder.id)}
+                </span>
+                {folder.name}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+        <div className='NoteListNav__button-wrapper'>
+          <CircleButton
+            tag={Link}
+            to='/add-folder'
+            type='button'
+            className='NoteListNav__add-folder-button'
+          >
+            <FontAwesomeIcon icon='plus' />
+            <br />
+            Folder
+          </CircleButton>
+          {this.state.adding && (
+            <AddFolder handleAdding={this.addFolderHandler} />
+          )}
+        </div>
+      </div>
+    );
+  }
+}
